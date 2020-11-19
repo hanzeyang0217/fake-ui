58e80124226725e8253f901f4015471809f8756f
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-19 10:45:32 +0900

    add home style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>fake-ui</h1>
       <h2>练习vue3用</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com/hanzeyang0217/fake-ui">GitHub</a>
+        <router-link to="doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -23,5 +23,25 @@
 
 <style lang="scss" scoped>
   .topnar{border: 1px solid red;}
-
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
