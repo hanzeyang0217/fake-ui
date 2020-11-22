cdd111042534fff29787c5807e72c471e4510d5a
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-22 18:02:04 +0900

    初步实现开关按钮

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,55 @@
 <template>
   <div>Switch本体</div>
+  <button @click="toggle" :class="{checked}">
+    <span></span>
+  </button>
 </template>
 
 <script lang="ts">
+  import {ref} from 'vue';
+
   export default {
-    name: 'Switch'
+    name: 'Switch',
+    setup() {
+      const checked = ref(false);
+      const toggle = () => {
+        checked.value = !checked.value;
+      };
+      return {checked, toggle};
+    }
   };
 </script>
 
 <style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: #999999;
+    border-radius: $h/2;
+    position: relative;
+    outline: none;
+  }
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+
+  button.checked {
+    > span {
+      background: #1976d2;
+      left: calc(100% - #{$h2} - 2px);
+    }
+    background-color: #8cbbe9;
+  }
 
 </style>
