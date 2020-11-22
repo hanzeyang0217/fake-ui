458370f0266c351bf6c5f90d78aa90e86f2c255d
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-22 17:27:58 +0900

    fix style

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="layout">
+    <Topnav class="nav"/>
     <div class="content">
       <aside v-if="menuVisible===true">
         <h2>组件列表</h2>
@@ -47,7 +47,7 @@
       provide('menuVisible', menuVisible);
 
       router.afterEach(() => {
-        if (!r.over600){
+        if (!r.over600) {
           menuVisible.value = false;
         }
       });
@@ -61,14 +61,48 @@
 </script>
 
 <style lang="scss" scoped>
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+
+    > .nav {
+      flex-shrink: 0;
+    }
+
+    > .content {
+      flex-grow: 1;
+      padding-top: 60px;
+      padding-left: 156px;
+      @media (max-width: 600px) {
+        padding-left: 0;
+      }
+    }
+  }
+
+  .content {
+    display: flex;
+
+    > aside {
+      flex-shrink: 0;
+    }
+
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+    }
+  }
+
   aside {
     background: lightblue;
     width: 150px;
     padding: 16px;
-    position: fixed;
     top: 0;
     left: 0;
     padding-top: 70px;
+    position: fixed;
+    height: 100%;
 
     > h2 {
       margin-bottom: 4px;
@@ -81,9 +115,9 @@
     }
   }
 
-  @media (max-width: 600px) {
-    aside {
-      /*display: none;*/
-    }
+  main {
+    top: 0;
+    overflow: auto;
   }
+
 </style>
