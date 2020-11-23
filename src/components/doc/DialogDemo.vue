fd121e3c709494a2718780e815af87e31ec32ac9
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-23 12:39:52 +0900

    add Dialog html

--- src/components/doc/DialogDemo.vue
+++ src/components/doc/DialogDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     Dialog 的文档
+    <h1>示例1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
 
 <script lang="ts">
+  import Dialog from '../../lib/Dialog.vue';
+
   export default {
-    name: 'DialogDemo'
+    name: 'DialogDemo',
+    components: {Dialog},
   };
 </script>
 
