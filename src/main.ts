ac6be53fb948d1d76588a035631f467a162ba21e
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-26 10:41:56 +0900

    用了第三方库github-markdown-css 引入了Github 的 Markdown 样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.scss';
 import {router} from './router/router';
+import 'github-markdown-css';
 
 createApp(App)
   .use(router)
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,9 +1,12 @@
 <template>
-  <article>
+  <article class="markdown-body">
     <h1>介绍</h1>
     <p>fake UI 是一款基于 Vue3 和 TypeScript 的 UI 组件库.</p>
     <p>这款组件库其实是我为了 学习vue3 而写的.</p>
-    <p>所以如果强烈不建议你将此 UI 库用于生产环境。</p>
+    <p>
+      <strong>所以如果强烈不建议你将此 UI 库用于生产环境。</strong>
+    </p>
+
     下一节：<a href="#/doc/install">安装</a>
   </article>
 </template>
